9ead00b226f1def15044de0b38ebecb04d6fbe58
Raymond Truong <37949023+raytru@users.noreply.github.com>
2021-11-04 03:23:30 +1100

    adding gui

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const choices = ["rock", "paper", "scissors"];
+const result_div = document.querySelector("#result");
+let userScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
   let randomElement = choices[Math.floor(Math.random() * choices.length)];
@@ -11,28 +14,47 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
-    return "You win";
+    userScore++;
+    result_div.innerHTML = playerSelection + " beats " + computerSelection + ". You win!";
+    updateScore();
   } else if (
     (computerSelection === "rock" && playerSelection === "scissors") ||
     (computerSelection === "scissors" && playerSelection === "paper") ||
     (computerSelection === "paper" && playerSelection === "rock")
   ) {
-    return "You lose";
+    computerScore++;
+    result_div.innerHTML = computerSelection + " beats " + playerSelection + ". You win!";
+    updateScore();
   } else {
-    return "It's a tie! Nobody wins";
+    result_div.innerHTML = "It's a tie! Nobody wins";
   }
 }
 
-function game() {
-  //loops 5 times for 5 rounds
-  for (var i=0; i < 5; i++) {
-    //gives user window prompt to enter selection
-    let playerSelection = prompt("select move");
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-    console.log()
-  }
-}
+const rockBtn = document.querySelector(".rockBtn");
+rockBtn.addEventListener("click", () => {
+  let playerSelection = "rock";
+  const computerSelection = computerPlay();
+  playRound(playerSelection, computerSelection);
+});
 
-//start the game
-game();
+const paperBtn = document.querySelector(".paperBtn");
+paperBtn.addEventListener("click", () => {
+  let playerSelection = "paper";
+  const computerSelection = computerPlay();
+  playRound(playerSelection, computerSelection);
+});
+
+const scissorBtn = document.querySelector(".scissorBtn");
+scissorBtn.addEventListener("click", () => {
+  let playerSelection = "scissors";
+  const computerSelection = computerPlay();
+  playRound(playerSelection, computerSelection);
+});
+
+const updateScore = () => {
+  const user = document.querySelector(".player-score p");
+  const pcu = document.querySelector(".computer-score p");
+  user.textContent = userScore;
+  pcu.textContent = computerScore;
+};
+
